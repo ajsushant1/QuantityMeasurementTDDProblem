8dc43884a560dd295e1a95b84708b054fedc4a34
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-09 19:59:59 +0530

    ADDED: getFeetValue method and override equals method

--- src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/QuantityMeasurement.java
@@ -1,7 +1,37 @@
 package com.bridgelabz.quantitymeasurement;
 
+import java.util.Objects;
+
 public class QuantityMeasurement {
+
+    private double feetUnitValue;
+
     public static void main(String[] args) {
         System.out.println("/**************************/ WELCOME TO QUANTITY MEASUREMENT /**************************/");
+    }
+
+    //METHOD TO GET UNIT TYPE
+    public double getFeetValue(double feetValue) {
+        this.feetUnitValue = feetValue;
+        return feetUnitValue;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null)
+            return false;
+        if (this.getClass() == o.getClass())
+            return true;
+        if(o.equals(o))
+            return true;
+        QuantityMeasurement that = (QuantityMeasurement) o;
+        return Double.compare(that.feetUnitValue, feetUnitValue) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(feetUnitValue);
     }
 }
