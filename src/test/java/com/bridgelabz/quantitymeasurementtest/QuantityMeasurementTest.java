328a6cf1cc78c371d01e5e38359e3ad3983365d7
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-09 09:27:58 +0530

    ADDED: Welcome message

---
+++ src/test/java/com/bridgelabz/quantitymeasurementtest/QuantityMeasurementTest.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.quantitymeasurementtest;
+
+import com.bridgelabz.quantitymeasurement.QuantityMeasurement;
+import org.junit.Before;
+
+public class QuantityMeasurementTest {
+    QuantityMeasurement quantityMeasurement=new QuantityMeasurement();
+}
