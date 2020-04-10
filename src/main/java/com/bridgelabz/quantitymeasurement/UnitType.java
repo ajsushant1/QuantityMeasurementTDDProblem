dd9bbfa8927aa954324f423ae103062f67d2636a
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-10 15:40:04 +0530

    ADDED: Enum constant for CENTIMETER

--- src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.quantitymeasurement;
 
 public enum UnitType {
-    FEET(12), INCH(1), YARD(36);
+    FEET(12), INCH(1), YARD(36), CENTIMETER(0.4);
 
     public double conversionUnit;
 
