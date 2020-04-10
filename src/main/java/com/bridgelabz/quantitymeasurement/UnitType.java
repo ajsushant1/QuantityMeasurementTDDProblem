2348da08d72c3611555720f1c81abfa54cba5ff2
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-10 23:56:23 +0530

    ADDED: Enum constant for TONNE

--- src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
@@ -3,7 +3,7 @@ package com.bridgelabz.quantitymeasurement;
 public enum UnitType {
     FEET(12), INCH(1), YARD(36), CENTIMETER(0.4),
     GALLON(3.78), LITRE(1), MILLILITRE(0.001),
-    KILOGRAM(1), GRAMS(0.001);
+    KILOGRAM(1), GRAMS(0.001), TONNE(1000);
 
     public double conversionUnitValue;
 
