6fb9d160159c8e5a00bed9bd37153453d929254d
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-10 22:19:34 +0530

    ADDED: Enum constant for GALLON and LITRE

--- src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.quantitymeasurement;
 
 public enum UnitType {
-    FEET(12), INCH(1), YARD(36), CENTIMETER(0.4);
+    FEET(12), INCH(1), YARD(36), CENTIMETER(0.4),
+    GALLON(3.78), LITRE(1);
 
     public double conversionUnitValue;
 
