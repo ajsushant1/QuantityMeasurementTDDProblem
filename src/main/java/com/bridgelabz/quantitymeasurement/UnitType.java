b92111e728b6e043ba62d3fd73d3eccb30a10d32
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-04-11 09:51:14 +0530

    ADDED: Enum constants for temperature

--- src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
+++ src/main/java/com/bridgelabz/quantitymeasurement/UnitType.java
@@ -3,7 +3,8 @@ package com.bridgelabz.quantitymeasurement;
 public enum UnitType {
     FEET(12), INCH(1), YARD(36), CENTIMETER(0.4),
     GALLON(3.78), LITRE(1), MILLILITRE(0.001),
-    KILOGRAM(1), GRAMS(0.001), TONNE(1000);
+    KILOGRAM(1), GRAMS(0.001), TONNE(1000),
+    FAHRENHEIT(1), CELSIUS(1.8);
 
     public double conversionUnitValue;
 
